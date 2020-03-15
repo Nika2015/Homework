4e1f2cfff754d75d450f7ad2f5e9243f6b141a19
ducho <ducho81@mail.ru>
2020-03-15 13:37:52 +0400

    Nightmare 2 /1

--- Nightmare2/src/Nigthmare2.java
+++ Nightmare2/src/Nigthmare2.java
@@ -2,15 +2,27 @@ public class Nigthmare2 {
     public static void main(String[] args) {
         String s = "1359765421";
         char[] chars = s.toCharArray();
-        int a=0;
-       char b=chars[0];
-        boolean library=false;
-        for (int i = chars.length - 1; i >= 1; i-- )
-        { if( chars[i-1]<chars[i]){  library= true;
-        a= i-1; b= chars[i-1];break;}}
-        if (!library)
-        {for( int i= 0; i< chars.length/2; i++)
-        {char t= chars[chars.length-1-i]; chars[chars.length-1-i]=chars[i]; chars[i]=t;}}
+        int a = 0;
+        char b = chars[0];
+        boolean library = false;
+        // gtnumenq vor elementic piti shrjvi-tvjal depqum chars[2]
+        for (int i = chars.length - 1; i >= 1; i--) {
+            if (chars[i - 1] < chars[i]) {
+                library = true;
+                a = i - 1;
+                b = chars[i - 1];
+                break;
+            }
+        }
+        // ete ajdpisi element chi gtnum  amboxchy shrjumenq
+        if (!library) {
+            for (int i = 0; i < chars.length / 2; i++) {
+                char t = chars[chars.length - 1 - i];
+                chars[chars.length - 1 - i] = chars[i];
+                chars[i] = t;
+            }
+        }
+        // hakarak depqum sksatc shrjman elementic dasavorumenq poqric metc
         else {
             for (int i = a; i < chars.length; i++) {
                 for (int j = a; j < chars.length - 1; j++) {
@@ -19,13 +31,17 @@ public class Nigthmare2 {
                         chars[j] = chars[j + 1];
                         chars[j + 1] = t;
                     }
-                }}
+                }
+            }
+            // voroshumenq hajord arjeqy chars[2] elementi, fiksumenq u ajd elementy zrojacnumenq
             for (int i = a; i < chars.length; i++) {
-                if (b == chars[i] && chars[i]!=chars[i+1]) {
+                if (b == chars[i] && chars[i] != chars[i + 1]) {
                     b = chars[i + 1];
                     chars[i + 1] = '0';
                     break;
-                }}
+                }
+            }
+            //mi hatelenq sortavorum sksatc chars[2]-ic poqric metc -zrojacatc elementy kga  chars[2] texy
 
             for (int i = a; i < chars.length; i++) {
                 for (int j = a; j < chars.length - 1; j++) {
@@ -34,15 +50,13 @@ public class Nigthmare2 {
                         chars[j] = chars[j + 1];
                         chars[j + 1] = t;
                     }
-                }}
-            chars[a]=b;
-
-
-
-
+                }
             }
-        System.out.println(chars);
+            //veragrumenq chars[2]-in zroi poxaren pahatc arjeqy
+            chars[a] = b;
         }
+        System.out.println(chars);
+    }
 }
 
 
